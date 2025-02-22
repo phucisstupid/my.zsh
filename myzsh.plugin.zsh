8669aa0b2143d86695ad647fdb4311cf29cc0280
phuc le <dog@192.168.1.22.non-exists.ptr.local>
2025-02-22 17:21:03 +0700

    update zsh

--- myzsh.plugin.zsh
+++ myzsh.plugin.zsh
@@ -2,25 +2,23 @@
 # 🌙 Environment Variables                             #
 # ─────────────────────────────────────────────────────#
 export XDG_CONFIG_HOME="$HOME/.config"
-# eval "$(/opt/homebrew/bin/brew shellenv)"
+export PATH="/opt/homebrew/bin:$PATH"
 export HOMEBREW_NO_ANALYTICS=1
-# source /opt/homebrew/opt/zinit/zinit.zsh
-# export STARSHIP_CONFIG="$XDG_CONFIG_HOME/starship/starship.toml"
+source /opt/homebrew/opt/zinit/zinit.zsh
 
 # 🚀 Load Shell Prompt
 # eval "$(starship init zsh)"
-# eval "$(oh-my-posh init zsh --config $(brew --prefix oh-my-posh)/themes/pure.omp.json)"
+eval "$(oh-my-posh init zsh --config $(brew --prefix oh-my-posh)/themes/pure.omp.json)"
 
 # 🐢 Atuin (Better Shell History)
 eval "$(atuin init zsh)"
 
 # 🐄 Cowsay (For Fun)
-zinit depth"1" lucid nocd light-mode atload"joke" for phucleeuwu/cowsay.zsh 
+# zinit depth"1" lucid nocd light-mode atload"joke | lolcat" for phucleeuwu/cowsay
 
 # ─────────────────────────────────────────────────────#
 # 🔌 OhMyZsh + Zinit Turbo (Plugins, Aliases, Configs) #
 # ─────────────────────────────────────────────────────#
-
 zinit depth"1" wait lucid nocd light-mode for \
   atinit"zicompinit; zicdreplay; unalias zi" \
       zsh-users/zsh-syntax-highlighting \
@@ -50,4 +48,4 @@ zinit depth"1" wait lucid nocd light-mode for \
       OMZP::gitignore \
       OMZP::git \
       OMZP::git-auto-fetch \
-      OMZP::magic-enter 
+      OMZP::magic-enter
