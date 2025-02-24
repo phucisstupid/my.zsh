a7fb6f6427746d5e5ecde53226b6e6267e417dcc
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com>
2025-02-24 21:52:40 +0700

    Update myzsh.plugin.zsh

--- myzsh.plugin.zsh
+++ myzsh.plugin.zsh
@@ -1,22 +1,4 @@
 # ─────────────────────────────────────────────────────#
-# 🌙 Environment Variables                             #
-# ─────────────────────────────────────────────────────#
-export XDG_CONFIG_HOME="$HOME/.config"
-export PATH="/opt/homebrew/bin:$PATH"
-export HOMEBREW_NO_ANALYTICS=1
-source /opt/homebrew/opt/zinit/zinit.zsh
-
-# 🚀 Load Shell Prompt
-# eval "$(starship init zsh)"
-eval "$(oh-my-posh init zsh --config $(brew --prefix oh-my-posh)/themes/pure.omp.json)"
-
-# 🐢 Atuin (Better Shell History)
-eval "$(atuin init zsh)"
-
-# 🐄 Cowsay (For Fun)
-# zinit depth"1" lucid nocd light-mode atload"joke | lolcat" for phucleeuwu/cowsay
-
-# ─────────────────────────────────────────────────────#
 # 🔌 OhMyZsh + Zinit Turbo (Plugins, Aliases, Configs) #
 # ─────────────────────────────────────────────────────#
 zinit depth"1" wait lucid nocd light-mode for \
@@ -25,7 +7,7 @@ zinit depth"1" wait lucid nocd light-mode for \
   atload"_zsh_autosuggest_start" \
       zsh-users/zsh-autosuggestions \
   blockf atpull'zinit creinstall -q .' \
-      clarketm/zsh-completions \
+      zsh-users/zsh-completions \
   pick"themes/catppuccin_mocha-zsh-syntax-highlighting.zsh" \
       catppuccin/zsh-syntax-highlighting \
       jeffreytse/zsh-vi-mode \
@@ -48,4 +30,4 @@ zinit depth"1" wait lucid nocd light-mode for \
       OMZP::gitignore \
       OMZP::git \
       OMZP::git-auto-fetch \
-      OMZP::magic-enter
+      OMZP::magic-enter 
